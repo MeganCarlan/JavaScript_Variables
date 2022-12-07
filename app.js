bbfc3dcb923afcb772a049ac89e64ba3b0d1cfaf
mcarlan <mgcarlan@gmail.com>
2022-12-06 21:25:51 -0600

    complete

--- app.js
+++ app.js
@@ -3,8 +3,44 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+
 // Exercise 1
+
+const firstName = "Megan"
+let lastName = "Carlan";
+var age = 25;
+
+console.log(lastName);
+console.log(age);
+
+
 
 // Exercise 2
 
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+
+
 // Exercise 3
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
